584e9c5c6e7dd229614531dd2cea723eba99749b
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-04-17 21:11:37 -0700

    Require Math

---
+++ week1/math/app.js
@@ -0,0 +1,13 @@
+const math = require("./math");
+
+console.log(math.add(10, 30));
+// 40
+
+console.log(math.subtract(20, 4));
+// 16
+
+console.log(math.multiply(10, 5));
+// 50
+
+console.log(math.divide(20, 2));
+// 10
